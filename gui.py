3bd4799209b4e85c2b0260a55f822913d47aca34
shr3sth <shresthdehuliya9654@gmail.com>
2026-05-31 18:34:15 +0530

    Connect system monitor to GUI

--- gui.py
+++ gui.py
@@ -1,4 +1,17 @@
 import tkinter as tk
+from tkinter import messagebox
+
+from modules.system_monitor import get_system_info
+
+
+def show_system_monitor():
+
+    info = get_system_info()
+
+    messagebox.showinfo(
+        "System Resource Monitor",
+        info
+    )
 
 
 root = tk.Tk()
@@ -16,13 +29,26 @@ title_label = tk.Label(
 title_label.pack(pady=20)
 
 
-tk.Button(root, text="Port Scanner").pack(pady=5)
+tk.Button(
+    root,
+    text="Port Scanner"
+).pack(pady=5)
 
-tk.Button(root, text="System Monitor").pack(pady=5)
+tk.Button(
+    root,
+    text="System Monitor",
+    command=show_system_monitor
+).pack(pady=5)
 
-tk.Button(root, text="File Integrity Monitor").pack(pady=5)
+tk.Button(
+    root,
+    text="File Integrity Monitor"
+).pack(pady=5)
 
-tk.Button(root, text="Log Analyzer").pack(pady=5)
+tk.Button(
+    root,
+    text="Log Analyzer"
+).pack(pady=5)
 
 
 root.mainloop()
